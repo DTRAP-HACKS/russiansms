0fc831f8c7b9dcf1b098323ff9d7712217500f10
MaksPV <51298369+MaksPV@users.noreply.github.com>
2020-05-07 22:43:12 +0300

    Add files via upload

--- generate.py
+++ generate.py
@@ -4,23 +4,29 @@ def count_lines(filename, chunk_size=1<<13):
                    for chunk in iter(lambda: file.read(chunk_size), ''))
 
 def generate(mode, name):
+	a = 1
 	f = open(name)
-	file = "import requests\n\ndef send(serv, _phone):\n"
+	file1 = ""
+	file = "import requests, random\n\ndef send(_phone):\n"
 	q = count_lines(name) - 1
 	for i in range(count_lines(name)):
 		line = f.readline()
 		if line[:2] == "p:":
 			file += "    " + line[3:]
 		elif line[:2] == "c:": print(line[3:])
-		else: file += "    if serv == " + str(q) + ": requests." + line
+		else:
+			file1 += "    if serv == " + str(q) + ": requests." + line
+			if a == 1:
+				b = q
+				a = 0
 		q -= 1
+	file += "    serv = random.randint(0, " + str(b) + ")\n"
+	file += file1
 	if mode == "s":
 		sms = open("sms.py", "w")
 		sms.write(file)
 		sms.close
-	if mode == " c":
+	if mode == "c":
 		calls = open("calls.py", "w")
 		calls.write(file)
 		calls.close
-	
-generate("s", "database.arsdat")
