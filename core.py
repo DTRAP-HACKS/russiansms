f6f00532824dfafc547a397cd2e2a1feefe35b68
MaksPV <51298369+MaksPV@users.noreply.github.com>
2020-05-07 20:45:46 +0300

    Add files via upload

--- core.py
+++ core.py
@@ -2,15 +2,25 @@ import sms, calls
 from random import randint
 from time import sleep
 
+def try_sms(_phone):
+    try:
+        sms.send(_phone)
+        return true
+    except:
+        return false
+
+def try_calls(_phone):
+    try:
+        calls.send(_phone)
+        return true
+    except:
+        return false
+
 def main(_phone, _quantity, _time, _percent):
-	while _quantity != 0:
-		if randint(0, 100) >= _percent:
-			try:
-				sms.send(_phone)
-				_quantity -=1
-		else:
-			try:
-				calls.send(_phone)
-				_quantity -= 1
-		
-		sleep(_time)
+    while _quantity != 0:
+        if randint(0, 100) >= _percent:
+            if try_sms(_phone) == true: _quantity -=1
+        else:
+            if try_calls(_phone) == true: _quantity -=1
+        
+        sleep(_time)
